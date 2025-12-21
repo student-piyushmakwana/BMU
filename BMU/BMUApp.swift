1d8cf69153a75a0c5b43109cb3ffe7e529bf74ab
Piyush Makwana <student.piyushmakwana@gmail.com>
2025-12-21 19:51:39 +0530

    feat: Initialize new BMU iOS application project with base files and configurations.

---
+++ BMU/BMUApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMUApp.swift
+//  BMU
+//
+//  Created by Piyush Makwana on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BMUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
